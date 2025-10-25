8e3acec2ca169fbc8265562bb4ac4f0a3c77c954
tomhuyquang@gmail.com <tomhuyquang@gmail.com>
2025-10-25 09:25:24 +0700

    add env

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,13 +3,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCs-B-pdzT1ixdurz6LY4bZS0hIEVQlDbA",
-  authDomain: "vnr202-63db3.firebaseapp.com",
-  projectId: "vnr202-63db3",
-  storageBucket: "vnr202-63db3.firebasestorage.app",
-  messagingSenderId: "1023581168149",
-  appId: "1:1023581168149:web:8b0df367f716f56599a41c",
-  measurementId: "G-R2YSRVN716",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // ⚡ Sửa ở đây: Kiểm tra nếu Firebase đã khởi tạo thì dùng lại
